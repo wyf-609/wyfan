dead83f1248b7b0413ab3099ae3c10d56e8edffc
wyf-609 <2505218516@qq.com>
2026-02-19 23:16:55 +0800

    feat: first commit

---
+++ clm/include/define.h
@@ -0,0 +1,21 @@
+#define	USGS_CLASSIFICATION       
+#undef	RDGRID                    
+#undef	RAWdata_update            
+#undef	DYN_PHENOLOGY             
+#undef	SOILINI                   
+#define	LANDONLY                  
+#undef	LAND_SEA                  
+#undef	SOIL_REFL_GUESSED         
+#define	SOIL_REFL_READ            
+#define	WO_DAILY           
+#define	WR_MONTHLY           
+#undef	CLMDEBUG                  
+#define	USE_CRUNCEP_DATA          
+#define	HEIGHT_V 100.        
+#define	HEIGHT_T 50.        
+#define	HEIGHT_Q 50.        
+#define	OPENMP 28                 
+#define NETCDF                    
+#define BGC
+#define BGC                       
+#undef  CaMa_Flood
